84f703d698f0e64120488266a8f1aaa9320a5333
euan.gethin <euan.gethin@ecotricity.co.uk>
2022-08-22 17:12:04 +0100

    ImageClassifier.py and DataFolderRequisites.py

--- DataFolderRequisites.py
+++ DataFolderRequisites.py
@@ -1,8 +1,13 @@
+# Sorts data directory based on classes and randomly sorts images into train/test/validation
+
 import os
 import random
 import pathlib
 import shutil
 import matplotlib.pyplot as plt
+import time
+from progress.bar import Bar
+
 
 def setup_folder_stuctrure():
     # Create base folders if they don't exist
@@ -20,13 +25,98 @@ def setup_folder_stuctrure():
     # Print the directory structure
     # Credits - https://stackoverflow.com/questions/3455625/linux-command-to-print-directory-structure-in-the-form-of-a-tree
     dir_str = os.system('''ls -R data | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/' ''')
-    # doesn't this only work on linux
+    # won't output properly on windows
     print(dir_str)
     return
 
-setup_folder_stuctrure()
+def train_test_validation_split(src_folder: pathlib.PosixPath, class_name: str):
+    # 80:10:10 split for train, test, validation
+    # use random number to put items into categories
+
+    # For tracking
+    n_train, n_valid, n_test = 0, 0, 0
+
+    # Random seed for reproducibility
+    random.seed(42)
+    count=0
+
+    # Scrollbar
+    bar = Bar("Processing", max=12501)
+
+    # Iterate over every image
+    for file in src_folder.iterdir():
+        img_name = str(file).split("\\")[-1]
+        #NOTE : ON WINDOWS SPLIT BY "\\", LINUX: "/"
 
 
+        # Make sure it's JPG
+        if file.suffix == ".jpg":
+
+            x = random.random()
+
+            # Choosing where image goes in folder
+            tgt_dir = ''
+
+            # .80 or below
+            if x <= pct_train:
+                tgt_dir = 'train'
+                n_train += 1
+
+            # Between .80 and .90
+            elif pct_train < x <= (pct_train + pct_valid):
+                tgt_dir = 'validation'
+                n_valid += 1
+
+            # Above .90
+            else:
+                tgt_dir = 'test'
+                n_test += 1
+
+            # Copy the image
+            shutil.copy(
+                src=file,
+                # data/<train|valid|test>/<cat\dog>/<something>.jpg
+                dst=f'{str(dir_data)}\\{tgt_dir}\\{class_name}\\{img_name}'
+            )
+
+        count+=1
+        bar.next()
+        #if count % 100 == 0:
+            #print(count, "/", len(os.listdir(src_folder)))
+
+    bar.finish()
+    return {
+        'source': str(src_folder),
+        'target': str(dir_data),
+        'n_train': n_train,
+        'n_validaiton': n_valid,
+        'n_test': n_test
+    }
+
+def plot_random_sample(img_dir: pathlib.PosixPath):
+    # How many images we're showing
+    n = 10
+    # Get absolute paths to these N images
+    imgs = random.sample(list(img_dir.iterdir()), n)
+
+    # Make sure num_row * num_col = n
+    num_row = 2
+    num_col = 5
+
+    # Create a figure
+    fig, axes = plt.subplots(num_row, num_col, figsize=(3.5 * num_col, 3 * num_row))
+    # For every image
+    for i in range(num_row * num_col):
+        # Read the image
+        img = plt.imread(str(imgs[i]))
+        # Display the image
+        ax = axes[i // num_col, i % num_col]
+        ax.imshow(img)
+        # Set title as <train|test|validation>/<cat\dog>/<img_name>.jpg
+        ax.set_title('/'.join(str(imgs[i]).split('/')[-3:]))
+
+    plt.tight_layout()
+    plt.show()
 
 
 # Distinct image classes
@@ -42,3 +132,29 @@ dir_test = dir_data.joinpath("test")
 pct_train = 0.8
 pct_valid = 0.1
 pct_test = 0.1
+
+setup_folder_stuctrure()
+
+print("Pathlib Woah", pathlib.Path.cwd().joinpath("PetImages/Cat"))
+
+start=time.time()
+print(start)
+
+train_test_validation_split(
+    src_folder=pathlib.Path.cwd().joinpath("PetImages/Cat"),
+    class_name="cat"
+)
+end=time.time()
+print(end-start)
+
+
+start=time.time()
+print(start)
+train_test_validation_split(
+    src_folder=pathlib.Path.cwd().joinpath("PetImages/Dog"),
+    class_name="dog"
+)
+print(time.time()-start)
+
+print("Finished")
+plot_random_sample(img_dir=pathlib.Path().cwd().joinpath('data/train/cat'))
--- ImageClassifier.py
+++ ImageClassifier.py
@@ -1,3 +1,70 @@
 # A combination of convolutions and pooling to classify images
 # ANNs are terrible for this, but CNNs are good
 
+# Remember need to remove a couple of images, one was 666
+
+import os
+import pathlib
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+tf.random.set_seed(42)
+
+from PIL import Image, ImageOps
+import warnings
+warnings.filterwarnings("ignore")
+
+def visualize_batch(batch: tf.keras.preprocessing.image.DirectoryIterator):
+    n = 64
+    num_row, num_col = 8, 8
+    fig, axes = plt.subplots(num_row, num_col, figsize=(3 * num_col, 3 * num_row))
+
+    for i in range(n):
+        img = np.array(batch[0][i] * 255, dtype='uint8')
+        ax = axes[i // num_col, i % num_col]
+        ax.imshow(img)
+
+    plt.tight_layout()
+    plt.show()
+
+img1 = Image.open('data/train/cat/1.jpg')
+print(np.array(img1).shape)
+
+img2 = Image.open('data/train/dog/0.jpg')
+print(np.array(img2).shape)
+
+# TensorFlow Data Loaders
+train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.0)
+valid_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1/255.0)
+
+train_data = train_datagen.flow_from_directory(
+    directory="data/train/",
+    target_size=(224, 224), # size all images will be resized to
+    class_mode="categorical", # categorical: distinct image classification
+    batch_size=64, # images shown to neural network
+    seed=42
+)
+
+first_batch = train_data.next() # 1 batch = 64 images
+print(first_batch[0].shape, first_batch[1].shape)
+
+#visualize_batch(batch=first_batch)
+
+# Train CNN
+train_data = train_datagen.flow_from_directory(
+    directory="data/train/",
+    target_size=(224,224),
+    class_mode="categorical",
+    batch_size=64,
+    seed=42
+)
+
+valid_datagen = valid_datagen.flow_from_directory(
+    directory="data/validation/",
+    target_size=(224,224),
+    class_mode="categorical",
+    batch_size=64,
+    seed=42
+)
